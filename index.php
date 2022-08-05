fbfb417b46c053d902896329d6ba0227452e10dc
juan <jadevlaplata@gmail.com>
2022-08-05 13:55:05 -0300

    corrección

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-require '/vendor/autoload.php';
+require 'vendor/autoload.php';
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
